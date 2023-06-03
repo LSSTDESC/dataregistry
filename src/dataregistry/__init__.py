2635a47a7330a7b736454d79c30862d5058e743f
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2023-06-03 18:05:38 +0200

    Updated the automatic versioning in pyproject.toml

--- src/dataregistry/__init__.py
+++ src/dataregistry/__init__.py
@@ -1,11 +1,4 @@
-try:
-    # For Python >= 3.8
-    from importlib import metadata
-except ImportError:
-    # For Python < 3.8
-    import importlib_metadata as metadata
-
-__version__ = metadata.version("dataregistry")
-
+from .version import __version__
 from .db_basic import *
 from .registrar import *
+
