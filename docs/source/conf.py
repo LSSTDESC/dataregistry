757ef7db037aba935d1b92ad35a9ea74c4aaeff1
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2023-06-08 14:25:03 +0200

    Added read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,35 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sphinx_rtd_theme
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Add the theme to the extensions list
+extensions = [
+    "sphinx_rtd_theme",
+]
 
-project = 'desc_data_management'
-copyright = '2023, DESC CO team'
-author = 'DESC CO team'
+project = 'DESC data management'
+copyright = '2023, DESC'
+author = 'DESC CI Working Group'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Set the theme
+html_theme = "sphinx_rtd_theme"
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-extensions = []
+# Optionally, you can customize the theme further with theme-specific options
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
 html_static_path = ['_static']
+html_logo = '_static/desc_logo.png'
