9d98dab303c75208de89a4879833573b4025ed7f
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2023-06-29 09:55:43 +0200

    Added query test for CLI entries

---
+++ tests/ci/create_test_entries_cli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dregs register my_cli_dataset "0.0.1" --is_dummy
+dregs register my_cli_dataset2 "patch" --is_dummy --name my_cli_dataset
--- tests/ci/test_cli.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dregs register my_cli_dataset "0.0.1" --is_dummy
