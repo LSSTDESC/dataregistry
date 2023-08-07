5e5e553514a14b8935a0aab96f2e7e165fc29f29
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2023-08-07 08:10:04 +0200

    Update DREGS class doc string

--- src/dataregistry/DREGS.py
+++ src/dataregistry/DREGS.py
@@ -6,8 +6,31 @@ from dataregistry.registrar import Registrar
 
 
 class DREGS:
+    def __init__(
+        self, config_file=None, schema_version=None, root_dir=None, verbose=False
+    ):
+        """
+        Primary data registry (DREGS) wrapper class.
 
-    def __init__(self, config_file=None, schema_version=None, root_dir=None, verbose=False):
+        Class links to both the Registrar class, to registry new dataset, and
+        the Query class, to query existing datasets.
+
+        Links to the database is done automatically using the default
+        assumptions (i.e., the users config file is in the default location and
+        the schema to connect to and the root directory are the defaults.
+        However each can also be manually specified. 
+
+        Parameters
+        ----------
+        config_file : str
+            Path to config file, if None, default location is assumed.
+        schema_version : str
+            Schema to connect to, if None, default schema is assumed.
+        root_dir : str
+            Root directory for DREGS datasets, if None, default is assumed.
+        verbose : bool
+            True for more output.
+        """
 
         # If no schema specified, go to the default one
         if schema_version is None:
@@ -22,7 +45,9 @@ class DREGS:
         self.Query = Query(engine, dialect, schema_version=self.schema_version)
 
         # Create registrar object
-        self.Registrar = Registrar(engine, dialect, schema_version=self.schema_version, root_dir=root_dir) 
+        self.Registrar = Registrar(
+            engine, dialect, schema_version=self.schema_version, root_dir=root_dir
+        )
 
     def gen_filter(self, property_name, bin_op, value):
         """
@@ -49,8 +74,5 @@ class DREGS:
         - : namedtuple
             The filter tuple
         """
-        
-        return Filter(property_name, bin_op, value)
 
-if __name__ == "__main__":
-    dregs = DREGS()
+        return Filter(property_name, bin_op, value)
