e772d23c8cac056bc0176c7cbfe8ad572f39767d
Stuart McAlpine <stuart.mcalpine@fysik.su.se>
2023-11-21 15:47:54 +0100

    Add root_dir to cli for creating datasets, needed for Sqlite

--- tests/end_to_end_tests/create_test_entries_cli.sh
+++ tests/end_to_end_tests/create_test_entries_cli.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # A basic entry
-dregs register dataset my_cli_dataset "0.0.1" --is_dummy
-dregs register dataset my_cli_dataset2 "patch" --is_dummy --name my_cli_dataset
+dregs register dataset my_cli_dataset "0.0.1" \
+    --is_dummy \
+    --root_dir "DataRegistry_data"
+dregs register dataset my_cli_dataset2 "patch" \
+    --is_dummy \
+    --name my_cli_dataset \
+    --root_dir "DataRegistry_data"
 
 # A basic entry with more options
 dregs register dataset my_cli_dataset3 "1.2.3" --is_dummy \
@@ -11,9 +16,11 @@ dregs register dataset my_cli_dataset3 "1.2.3" --is_dummy \
     --locale "Secret location" \
     --owner "DESC" \
     --owner-type "group" \
-    --version_suffix "test"
+    --version_suffix "test" \
+    --root_dir "DataRegistry_data"
 
 # A production dataset
 dregs register dataset my_production_cli_dataset "0.1.2" \
     --owner-type "production" \
-    --is_dummy
+    --is_dummy \
+    --root_dir "DataRegistry_data"
