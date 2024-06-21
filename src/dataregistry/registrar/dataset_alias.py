65d9c197c2651e2b902dec02b1b592ec710c5ca2
Joanne Bogart <jrb@slac.stanford.edu>
2024-06-21 15:17:34 -0700

    Add supersede argument to register, defaulting to False

--- src/dataregistry/registrar/dataset_alias.py
+++ src/dataregistry/registrar/dataset_alias.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from dataregistry.db_basic import add_table_row
-from sqlalchemy import update
+from sqlalchemy import update, select
 from .registrar_util import _read_configuration_file
 
 from .base_table_class import BaseTable
@@ -15,7 +15,8 @@ class DatasetAliasTable(BaseTable):
         self.entry_id = "dataset_alias_id"
 
     def register(self, aliasname, dataset_id, ref_alias_id=None,
-                 access_api=None, access_api_configuration=None):
+                 access_api=None, access_api_configuration=None,
+                 supersede=False):
         """
         Create a new `dataset_alias` entry in the DESC data registry.
         It may refer to a dataset (default) or another alias
@@ -33,7 +34,9 @@ class DatasetAliasTable(BaseTable):
         access_api**               : str  api, if any, which can read the
                                           dataset
         access_api_configuration** : str  extra information for access_api
-
+        supersede                  : bool if True, create a new entry with
+                                          this alias name even if old ones
+                                          exist
 
         Returns
         -------
@@ -61,7 +64,16 @@ class DatasetAliasTable(BaseTable):
         values["creator_uid"] = self._uid
 
         alias_table = self._get_table_metadata("dataset_alias")
+
+        # If not supersede, check if alias name has already been used
         with self._engine.connect() as conn:
+            if not supersede:
+                q = select(alias_table.c.aliasname).where(
+                    alias_table.c.aliasname == aliasname)
+                result = conn.execute(q)
+                if result.fetchone():
+                    print(f"Alias {aliasname} already exists. Specify 'supersede=True' to override")
+                    return None
             prim_key = add_table_row(conn, alias_table, values)
 
             # Update any other alias rows which have been superseded
