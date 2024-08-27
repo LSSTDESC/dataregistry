a751b01c99c3f3f63035b931655859e1be463e4b
Stuart McAlpine <stuart.mcalpine@fysik.su.se>
2024-08-27 14:26:49 +0200

    Fix test

--- tests/end_to_end_tests/test_register_dataset_real_data.py
+++ tests/end_to_end_tests/test_register_dataset_real_data.py
@@ -50,24 +50,15 @@ def test_copy_data(dummy_file, data_org):
 
 
 @pytest.mark.parametrize(
-    "data_org,data_path,v_str,overwritable",
+    "data_org,data_path",
     [
-        ("file", "file1.txt", "0.0.1", True),
-        ("file", "file1.txt", "0.0.2", True),
-        ("file", "file1.txt", "0.0.3", False),
-        ("directory", "dummy_dir", "0.0.1", True),
-        ("directory", "dummy_dir", "0.0.2", True),
-        ("directory", "dummy_dir", "0.0.3", False),
+        ("file", "file1.txt"),
+        ("directory", "dummy_dir"),
     ],
 )
-def test_on_location_data(dummy_file, data_org, data_path, v_str, overwritable):
+def test_on_location_data(dummy_file, data_org, data_path):
     """
-    Test ingesting real data into the registry (already on location). Also
-    tests overwriting datasets.
-
-    Does three times for each file, the first is a normal entry with
-    `is_overwritable=True`. The second and third tests overwriting the previous
-    data with a new version.
+    Test ingesting real data into the registry (already on location).
     """
 
     # Establish connection to database
@@ -77,10 +68,9 @@ def test_on_location_data(dummy_file, data_org, data_path, v_str, overwritable):
     d_id = _insert_dataset_entry(
         datareg,
         f"DESC:datasets:test_on_location_data_{data_org}",
-        v_str,
+        "0.0.1",
         old_location=None,
         location_type="dataregistry",
-        is_overwritable=overwritable,
         relative_path=data_path,
     )
 
@@ -90,43 +80,15 @@ def test_on_location_data(dummy_file, data_org, data_path, v_str, overwritable):
             "dataset.data_org",
             "dataset.nfiles",
             "dataset.total_disk_space",
-            "dataset.is_overwritable",
-            "dataset.is_overwritten",
-            "dataset.version_string",
         ],
         [f],
-        return_format="cursorresult",
     )
 
-    num_results = len(results.all())
-    for i, r in enumerate(results):
-        assert getattr(r, "dataset.data_org") == data_org
-        assert getattr(r, "dataset.nfiles") == 1
-        assert getattr(r, "dataset.total_disk_space") > 0
-        if getattr(r, "version_string") == "0.0.1":
-            if num_results == 1:
-                assert getattr(r, "dataset.is_overwritable") == True
-                assert getattr(r, "dataset.is_overwritten") == False
-            else:
-                assert getattr(r, "dataset.is_overwritable") == True
-                assert getattr(r, "dataset.is_overwritten") == True
-        elif getattr(r, "version_string") == "0.0.2":
-            assert num_results >= 2
-            if num_results == 2:
-                assert getattr(r, "dataset.is_overwritable") == True
-                assert getattr(r, "dataset.is_overwritten") == False
-            elif num_results == 3:
-                assert getattr(r, "dataset.is_overwritable") == True
-                assert getattr(r, "dataset.is_overwritten") == True
-        elif getattr(r, "version_string") == "0.0.3":
-            assert num_results >= 3
-            if num_results == 3:
-                assert getattr(r, "dataset.is_overwritable") == False
-                assert getattr(r, "dataset.is_overwritten") == False
-            else:
-                assert getattr(r, "dataset.is_overwritable") == True
-                assert getattr(r, "dataset.is_overwritten") == True
+    assert len(results["dataset.data_org"]) == 1
 
+    assert results["dataset.data_org"][0] == data_org
+    assert results["dataset.nfiles"][0] == 1
+    assert results["dataset.total_disk_space"][0] > 0
 
 @pytest.mark.parametrize("link", ["file1_sym.txt", "directory1_sym"])
 def test_registering_symlinks(dummy_file, link):
@@ -170,7 +132,7 @@ def test_registering_bad_relative_path(dummy_file, link):
         "0.0.1",
         old_location=data_path,
         location_type="dataregistry",
-        relative_path=f"my/relative/path/for/checking/{link}",
+        relative_path=f"test/register/bad/relpath/{link}",
     )
 
     with pytest.raises(ValueError, match="combination is already"):
@@ -180,7 +142,7 @@ def test_registering_bad_relative_path(dummy_file, link):
             "0.0.1",
             old_location=data_path,
             location_type="dataregistry",
-            relative_path=f"my/relative/path/for/checking/{link}",
+            relative_path=f"test/register/bad/relpath/{link}",
         )
 
 
