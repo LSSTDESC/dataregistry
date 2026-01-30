5630426be19afe02127c75acde619ba5840038c5
Joanne Bogart <jrb@slac.stanford.edu>
2026-01-30 12:12:08 -0800

    add debug code to a CI test

--- tests/end_to_end_tests/test_database_functions.py
+++ tests/end_to_end_tests/test_database_functions.py
@@ -50,10 +50,17 @@ def test_get_dataset_absolute_path(dummy_file, schema):
         )
     else:
         schema_name = datareg.db_connection._namespace + "_" + schema
-        assert v == os.path.join(
+        should_be = os.path.join(
             str(tmp_root_dir), schema_name, dset_ownertype, dset_owner,
             dset_relpath
             )
+        print(f"Returned from get_dataset_absolute_path: {v}")
+        print(f"Should be: {should_be}")
+        assert v == should_be
+        # assert v == os.path.join(
+        #     str(tmp_root_dir), schema_name, dset_ownertype, dset_owner,
+        #     dset_relpath
+        #     )
     # Cannot easily make a similar test for non-sqlite since schema type
     # ("production", "working") is not what's used to form the path; the
     # full schema name is.
